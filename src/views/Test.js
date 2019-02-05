943ca5211ceca1fbdbd8217878a5ab6fb49e7ee7
Daniel Weinman <namniewd@gmail.com>
2019-02-04 20:09:06 -0500

    netlify deploy complete

--- src/views/Test.js
+++ src/views/Test.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Test = props => {
     return (
-        <h1 onClick={() => props.history.push('/login')}>Test</h1>
+        <h1 onClick={() => props.history.push('/login')}>Click to go to Login Page</h1>
     )
 }
 
