82bc792af0c35b4dbd5f757e6b56bf57c6d93ac7
Daniel Weinman <namniewd@gmail.com>
2019-02-06 08:55:20 -0500

    added profile to SingleTripView

--- src/views/SingleTripView.js
+++ src/views/SingleTripView.js
@@ -2,8 +2,9 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 
-import { getTrips, getUser } from '../actions'
+import { getTrips, getUser, toggleEditUser, handleEditUserChanges, editUser } from '../actions'
 import Trip from '../components/Trip/Trip'
+import Profile from '../components/Profile/Profile'
 
 class SingleTripView extends React.Component {
     constructor(props){
@@ -35,16 +36,19 @@ class SingleTripView extends React.Component {
         if (id) {
             this.props.getUser(id);
             this.props.getTrips(id);
-        }
+        } 
     }
 
     findTrip = () => {
         const trip = this.state.trips.find(trip => this.props.match.params.id === `${trip.id}`)
         this.setState({trip})
     }
-    
-    
 
+    handleEditUserSubmit = (e, user) => {
+        e.preventDefault();
+        this.props.editUser(user);
+    }
+    
     render() {
         if (!localStorage.getItem('token')) {
             return (
@@ -55,13 +59,24 @@ class SingleTripView extends React.Component {
             )
         }
         return (
-            <Trip trip={this.state.trip} />
+            <>
+                <Profile 
+                    user={this.props.user} 
+                    isEditingUser={this.props.isEditingUser}
+                    toggleEditUser={this.props.toggleEditUser}
+                    handleEditUserChanges={this.props.handleEditUserChanges}
+                    handleEditUserSubmit={this.handleEditUserSubmit}
+                />
+                <Trip trip={this.state.trip} />
+            </>
         )
     }
 }
 
 const mapStateToProps = state => ({
-    trips: state.trips
+    trips: state.trips,
+    user: state.loggedInUser,
+    isEditingUser: state.isEditingUser,
 })
 
-export default connect(mapStateToProps, { getUser, getTrips })(SingleTripView)
+export default connect(mapStateToProps, { getUser, getTrips, toggleEditUser, handleEditUserChanges, editUser })(SingleTripView)
